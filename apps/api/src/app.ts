61423928aadd72a870a216c28c2be28586c05fae
Gideão <gideaoms@gmail.com>
2023-11-17 11:23:07 -0300

    change to dashboard name

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,26 +1,26 @@
-import "@total-typescript/ts-reset";
-import { APP_NODE_ENV } from "@/envs.js";
-import { db } from "@/libs/knex.js";
-import autoload from "@fastify/autoload";
-import cors from "@fastify/cors";
-import helmet from "@fastify/helmet";
-import fastify from "fastify";
-import path from "node:path";
-import url from "node:url";
+import '@total-typescript/ts-reset';
+import { APP_NODE_ENV } from '@/envs.js';
+import { db } from '@/libs/knex.js';
+import autoload from '@fastify/autoload';
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
+import fastify from 'fastify';
+import path from 'node:path';
+import url from 'node:url';
 
-await db.raw("select 1");
+await db.raw('select 1');
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export const app = fastify({
-  logger: APP_NODE_ENV === "development",
+  logger: APP_NODE_ENV === 'development',
 });
 
 app.register(helmet);
 app.register(cors);
 app.register(autoload, {
-  dir: path.join(dirname, "external", "controllers"),
-  options: { prefix: "/v1" },
+  dir: path.join(dirname, 'external', 'controllers'),
+  options: { prefix: '/v1' },
   dirNameRoutePrefix: false,
   forceESM: true,
 });
